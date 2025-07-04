f5986c57cfccba6bd8f8910a2c06218888a9af79
tranndt <tranndt@myumanitoba.ca>
2025-07-03 21:25:53 -0500

    fix: Update environment variables for production readiness and adjust server log output

--- server/database/app.js
+++ server/database/app.js
@@ -89,5 +89,5 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 
 // Start the Express server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://127.0.0.1:${port}`);
 });
